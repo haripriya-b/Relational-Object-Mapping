cf99b8e1450887fb78c7ef328e9cba2f580e0014
haripriya_b <hari2512@gmail.com>
2015-02-18 16:17:30 +0530

    class and DAO added

--- src/relational_to_or/ReverseEnggDAO.java
+++ src/relational_to_or/ReverseEnggDAO.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public interface ReverseEnggDAO {
-	public ArrayList<String> getObjects();
-	public Map<String, String> getAttributes(String className);
-	public Map<String, String> getPrimaryKeys();
-	public ForeignKeyDetails getFKs();
-	ArrayList<RelatedTables> getOneToOneAssociations();
-	ArrayList<RelatedTables> getOneToManyAssociation();
-	ArrayList<RelatedTables> getManyToManyAssociation();
-	ArrayList<InheritedTables> getInheritace();
-	ArrayList<RelatedTables> getComposition();
+	 ArrayList<String> getObjects();
+	 Map<String, String> getAttributes(String className);
+	 Map<String, String> getPrimaryKeys();
+	 ForeignKeyDetails getFKs();
+	 ArrayList<RelatedTables> getOneToOneAssociations();
+	 ArrayList<RelatedTables> getOneToManyAssociation();
+	 ArrayList<RelatedTables> getManyToManyAssociation();
+	 ArrayList<InheritedTables> getInheritace();
+	 ArrayList<RelatedTables> getComposition();
 
 }

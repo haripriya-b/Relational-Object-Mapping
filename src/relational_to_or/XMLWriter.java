602c6a903eda3874044de9e1813da7f579794f5b
haripriya_b <hari2512@gmail.com>
2015-03-17 23:12:02 +0530

    XMLWriter added

--- src/relational_to_or/XMLWriter.java
+++ src/relational_to_or/XMLWriter.java
@@ -10,6 +10,8 @@ import javax.xml.stream.events.StartDocument;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
+
+
 public class XMLWriter {
 	private ArrayList<String> configFiles;
 	
@@ -25,14 +27,24 @@ public class XMLWriter {
 		for(i=0;i<configFiles.size();i++) {
 			//Create XMLEventWriter
 			XMLEventWriter eventWriter = outputFactory.createXMLEventWriter(new FileOutputStream(configFiles.get(i)));
-			
+		
 			//Create EventFactory
 			XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 			XMLEvent end = eventFactory.createDTD("\n");
+			XMLEvent open = eventFactory.createDTD("<");
+			XMLEvent close = eventFactory.createDTD(">");
 			
 			//Create and write start tag
 			StartDocument startDocument = eventFactory.createStartDocument();
 			eventWriter.add(startDocument);
+			eventWriter.add(end);
+			
+			
+			//Create and write !DOCTYPE
+			eventWriter.add(open);
+			eventWriter.add(eventFactory.createCharacters( "!DOCTYPE hibernate-mapping PUBLIC \"-//Hibernate/Hibernate Mapping DTD//EN\"\"http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd\""));
+			eventWriter.add(close);
+			eventWriter.add(end);
 			
 			//Create hibernate-mapping open tag
 			StartElement hibernateStartElement = eventFactory.createStartElement("", "", "hibernate-mapping");
@@ -40,12 +52,19 @@ public class XMLWriter {
 			eventWriter.add(end);
 			
 			//Create the class tag
+			StartElement classStartElement = eventFactory.createStartElement("", "", "class");
+			eventWriter.add(classStartElement);
+			eventWriter.add(eventFactory.createAttribute("name", classes.get(i).getName()));
+			eventWriter.add(eventFactory.createAttribute("table",classes.get(i).getName()));
+			eventWriter.add(end);
 			
 			
 			
 			//Write the different nodes
 			
-			
+			//Create class close tag
+			eventWriter.add(eventFactory.createEndElement("", "", "class"));
+			eventWriter.add(end);
 			
 			// Create hibernate-mapping close tag
 			eventWriter.add(eventFactory.createEndElement("", "", "hibernate-mapping"));

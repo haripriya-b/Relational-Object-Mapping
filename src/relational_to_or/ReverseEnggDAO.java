f1493b55c38552e9fa1032fd6f72c533fad69cac
Anusha P S <anusha.ps27@gmail.com>
2015-04-09 00:06:30 +0530

    DAO updated

--- src/relational_to_or/ReverseEnggDAO.java
+++ src/relational_to_or/ReverseEnggDAO.java
@@ -8,10 +8,11 @@ public interface ReverseEnggDAO {
 	 ArrayList<Attribute> getAttributes(String className);
 	 ArrayList<Attribute> getPrimaryKeys(String className);
 	 ArrayList<Class_Details> getClasses(ArrayList<String> classNames);
-	 ForeignKeyDetails getFKs();
-	 ArrayList<RelatedTables> getOneToOneAssociations();
-	 ArrayList<RelatedTables> getOneToManyAssociation();
-	 ArrayList<RelatedTables> getManyToManyAssociation();
-	 ArrayList<InheritedTables> getInheritace();
-	 ArrayList<RelatedTables> getComposition();
+	 ArrayList<Referential_Constraint> getAllConstraints();
+	 ArrayList<ManyToMany> getManyToManyRelations(ArrayList<Referential_Constraint> constraints, ArrayList<Class_Details> classes);
+	 ArrayList<Referential_Constraint> findManyToManyRelations(ArrayList<Referential_Constraint> constraints, ArrayList<Class_Details> classes);
+	 ArrayList<Referential_Constraint> findInheritance(ArrayList<Referential_Constraint> constraints);
+	 ArrayList<Referential_Constraint> findComposition(ArrayList<Referential_Constraint> constraints);
+	 ArrayList<Referential_Constraint> findOnetoOne(ArrayList<Referential_Constraint> constraints);
+	 ArrayList<Referential_Constraint> findOneToMany(ArrayList<Referential_Constraint> constraints);
 }

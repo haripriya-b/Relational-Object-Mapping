3bd28fbe86ebd4bd07a5bb7227d5a1c7fbca752e
Anusha P S <anusha.ps27@gmail.com>
2015-04-09 16:36:10 +0530

    Update

--- src/relational_to_or/Relation_Type.java
+++ src/relational_to_or/Relation_Type.java
@@ -1,5 +1,9 @@
 package relational_to_or;
 
 public enum Relation_Type {
-ONE_TO_ONE, ONE_TO_MANY, INHERITANCE, COMPOSITION, MANY_TO_MANY
+	ONE_TO_ONE,
+	ONE_TO_MANY,
+	INHERITANCE,
+	COMPOSITION,
+	MANY_TO_MANY
 }
